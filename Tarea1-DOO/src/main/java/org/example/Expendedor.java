813441c40050c9cb4f9dea30027973c9f201b32d
Paulo <paulotorrescavila@gmail.com>
2026-05-01 18:18:52 -0400

    refactor: implementar enumeracion de porductos

--- Tarea1-DOO/src/main/java/org/example/Expendedor.java
+++ Tarea1-DOO/src/main/java/org/example/Expendedor.java
@@ -5,6 +5,9 @@ public class Expendedor {
     private Deposito<Bebida> coca;
     private Deposito<Bebida> sprite;
     private Deposito<Moneda> monVu;
+    private Deposito<Bebida> fanta;
+    private Deposito<Dulce> super8;
+    private Deposito<Dulce> snicker;
     private int precio;
     public static final int COCA=1;
     public static final int SPRITE=2;
@@ -12,15 +15,22 @@ public class Expendedor {
         coca = new Deposito<Bebida>();
         sprite = new Deposito<Bebida>();
         monVu = new Deposito<Moneda>();
+        fanta = new Deposito<Bebida>();
+        super8 = new Deposito<Dulce>();
+        snicker = new Deposito<Dulce>();
+
         precio = precioBebidas;
         for(int i = 0 ; i < numBebidas ; i++){
             coca.addAlgo(new CocaCola(100 + i));
             sprite.addAlgo(new Sprite(200 + i));
+            fanta.addAlgo(new Fanta(300 + i));
+            super8.addAlgo(new Super8(400 + i));
+            snicker.addAlgo(new Snickers(500 + i));
 
         }
 
     }
-    public Bebida comprarBebida (Moneda m , int cual) throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
+    public Producto comprarBebida (Moneda m , Enumeracion cual) throws NoHayProductoException, PagoIncorrectoException, PagoInsuficienteException {
 
         if(m == null){
             throw new PagoIncorrectoException();
@@ -30,31 +40,26 @@ public class Expendedor {
             throw new PagoInsuficienteException();
 
         }
+        Producto p = null;
 
-        Bebida b = null;
-        if(cual == 1){
-            b = coca.getAlgo();
+        switch(cual){
+            case COCA_COLA -> p = coca.getAlgo();
+            case FANTA -> p = fanta.getAlgo();
+            case SPRITE ->  p= sprite.getAlgo();
+            case SUPER8 -> p= super8.getAlgo();
+            case SNICKERS -> p= snicker.getAlgo();
         }
-        else if(cual == 2){
-            b = sprite.getAlgo();
-        }
-        else{
-            monVu.addAlgo(m);
+        if (p == null){
+            monVu.addAlgo();
             throw new NoHayProductoException();
         }
-
-        if(b == null){
-            monVu.addAlgo(m);
-            throw new NoHayProductoException();
-        }
-
         int diferencia = m.getValor() - precio;
 
         for(int i = 0; i < diferencia; i += 100){
             monVu.addAlgo(new Moneda100());
         }
 
-        return b;
+        return p;
     }
 
     public Moneda getVuelto(){
--- Tarea1-DOO/src/main/java/org/example/Moneda1500.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class Moneda1500 {public Moneda1500(){
-    super();
-}
-    public int getValor(){
-        return 1500;
-    }
-}
